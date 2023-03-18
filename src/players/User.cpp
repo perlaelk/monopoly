a90ebe36dee839ed0e876fa3c5b30fe62f6fe5b0
Roland Aoun <roland.aoun@murex.com>
2023-03-18 15:27:17 +0200

    Removing tokhbis Perla

--- src/players/User.cpp
+++ src/players/User.cpp
@@ -4,16 +4,7 @@
 
 
 using namespace players;
-class Player
+std::string User::printSomething() const
 {
-	public:
-		Player::Player(const char *name,int turn);
-		static int next_id;
-		turn = -turn + 1;
-		Player::Player(std::string Playername);
-	
-	private:
-		char* name_;
-		int turn;
-};
-
+   return "ma khassak";
+}
