7f9b4af738cd29884a44b209c315ef4f7f995f6c
Roland Aoun <roland.aoun@murex.com>
2023-03-18 14:31:24 +0200

    fixing Perla's tokhbisa

--- src/mechanics/Dice.cpp
+++ src/mechanics/Dice.cpp
@@ -6,19 +6,5 @@ using namespace mechanics;
 
 std::string Dice::printSomething() const
 {
-	class dice
-	{
-	public:
-		int number;
-		dice()
-		{
-			srand(time(NULL) + rand());
-			number = rand() % 6 + 1;
-		}
-		void roll()
-		{
-			srand(time(NULL) + rand());
-			number = rand() % 6 + 1;
-		}
-	};
+	return "ma khassik";
 }
